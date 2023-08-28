bf744c815a5c702d94d08711b1bf9cfd24408daf
ItsTheGuy <36568572+ItsTheGuy@users.noreply.github.com>
2023-08-28 20:40:55 +0200

    `storage::object::Object::find()`: Copies `id` and `value` into a heap allocated buffer to move it to the spawned task

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -21,3 +21,69 @@ use tokio::sync::OnceCell;
 use tokio::task::*;
 
 pub static DB: OnceCell<AtomicRefCell<Arc<Database>>> = OnceCell::const_new();
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn object() -> Result<()> {
+        // Create a temporal file to store the DB
+        let tmpfile = tempfile::NamedTempFile::new()?;
+
+        // Create a DB
+        DB.set(AtomicRefCell::new(Arc::new(Database::create(
+            tmpfile.path(),
+        )?)))?;
+
+        // Generate keys
+        let keys: SVec<UserID> = (0..32).map(|_| rand::random::<UserID>()).collect();
+
+        // Insert data
+        let mut inserts: JoinSet<Result<()>> = JoinSet::new();
+        for key in &keys {
+            inserts.spawn(Preferences::insert(*key, Preference::default()));
+        }
+        while inserts.join_next().await.is_some() {}
+
+        // Check whether all the data has been inserted
+        assert_eq!(
+            keys.len(),
+            DB.get()
+                .context("Cannot get DB handle")?
+                .borrow()
+                .begin_read()?
+                .open_table(Preferences::TABLE_DEFINITION)?
+                .len()? as usize
+        );
+
+        // Insert unique value
+        let (unique_id, unique_value) = (
+            rand::random::<UserID>(),
+            Preference::new(Some(Gender::Female), None, LocationRange::default()),
+        );
+        Preferences::insert(unique_id, unique_value.clone()).await?;
+
+        // Get the unique value
+        let retrieved_unique_value = Preferences::get(unique_id)
+            .await?
+            .context("Unique value not found!")?;
+
+        // Check whether the retrieved value is the unique value
+        assert!(
+            unique_value == retrieved_unique_value,
+            "Unique value and retrieved value aren't the same!"
+        );
+
+        // Search the unique value's id by itself
+        let found_id = Preferences::search(retrieved_unique_value, true)
+            .await?
+            .unwrap_left()
+            .context("Unique value's id not found!")?;
+
+        // Check whether the unique value's id and the id found are the same
+        assert_eq!(unique_id, found_id);
+
+        Ok(())
+    }
+}
